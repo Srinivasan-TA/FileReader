d12228fb5159ea24482120ad54d01d6926933421
tringapps-srinivasan <31205587+Srinivasan-TA@users.noreply.github.com>
2023-02-09 18:03:16 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String fileName = "C:\\Users\\Tringapps-user19\\Downloads\\sample.txt";
+        HashMap<String, Integer> newkey = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+                    if (newkey.containsKey(word)) {
+                        newkey.put(word, newkey.get(word) + 1);
+                    } else {
+                        newkey.put(word, 1);
+                    }
+                }
+            }
+        }
+        Map<String, Integer> sortedWordCounts = sortByValue(newkey);
+       for (Map.Entry<String, Integer> entry : sortedWordCounts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+    private static Map<String, Integer> sortByValue(Map<String, Integer> newkey) {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(newkey.entrySet());
+        list.sort((o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
+//        System.out.println("-----------"+list);
+
+        Map<String, Integer> words = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : list) {
+            words.put(entry.getKey(), entry.getValue());
+        }
+        return words;
+    }
+}
